bb86fa00ece711c17bd9fc3d66496effea1610fb
Sardor Bees <sardor@example.com>
2025-08-31 12:53:26 +0500

    Initial commit

--- src/components/owl/Owl.jsx
+++ src/components/owl/Owl.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../assets/css/style.css'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 function Owl() {
     return (
